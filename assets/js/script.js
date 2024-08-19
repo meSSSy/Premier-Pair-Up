258d5d114739aa185d4d359202915a50bad6ec35
Chris Parry <chrisparry07@live.co.uk>
2024-08-19 17:31:32 +0000

    Navbar, logo, background image and signup form added to landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,34 @@
 const cards = document.querySelectorAll('.crest-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // Check if the two selected cards match
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            // Disable further clicks on the matched cards
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            // Unflip the cards after a short delay
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1000);
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+// timer
+
